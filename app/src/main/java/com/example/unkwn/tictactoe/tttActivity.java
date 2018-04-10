85c9effbd0aa28efc634d5fb1a1155416fdabe58
MahmoudFarouk <20901567+MahmoudFarouq@users.noreply.github.com>
2018-04-10 22:00:05 +0200

    Update tttActivity.java
    note that on winning the game, the players can continue playing, this needs to be fixed :"D

--- app/src/main/java/com/example/unkwn/tictactoe/tttActivity.java
+++ app/src/main/java/com/example/unkwn/tictactoe/tttActivity.java
@@ -7,40 +7,29 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class tttActivity extends AppCompatActivity {
-    Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    Button[] buttons;
     int turn;
      public void checkGame(){
-        checkWinner(b1,b2,b3);
-        checkWinner(b4,b5,b6);
-        checkWinner(b7,b8,b9);
-        checkWinner(b1,b4,b7);
-        checkWinner(b2,b5,b8);
-        checkWinner(b3,b6,b9);
-        checkWinner(b1,b5,b9);
-        checkWinner(b3,b5,b7);
+         for(int i = 0; i < 9; i+=3){
+             checkWinner(buttons[i],buttons[i+1],buttons[i+2]);
+         }
+         for(int i = 0; i < 3; i++){
+             checkWinner(buttons[i],buttons[i+3],buttons[i+6]);
+         }
+        checkWinner(buttons[0],buttons[4],buttons[8]);
+        checkWinner(buttons[2],buttons[4],buttons[6]);
     }
 
     public void checkWinner(Button b1,Button b2,Button b3){
-        if (b1.getText()==b2.getText().toString() && b2.getText().toString()==b3.getText().toString() && b3.getText().toString()=="X"){
-            Toast.makeText(tttActivity.this,"player 1 is winner ",Toast.LENGTH_LONG).show();
-
-        }
-        if (b1.getText()==b2.getText().toString() && b2.getText().toString()==b3.getText().toString() && b3.getText().toString()=="O"){
-            //player 2 wins
-            Toast.makeText(tttActivity.this,"player 2 is winner ",Toast.LENGTH_LONG).show();
+        if (b1.getText().equals(b2.getText()) && b2.getText().equals(b3.getText()) && b3.getText().equals("X")){
+            Toast.makeText(tttActivity.this, String.format("player %s is winner ", turn),Toast.LENGTH_LONG).show();
         }
     }
     public void reset(View view){
         turn =1;
-        b1.setText("");
-        b2.setText("");
-        b3.setText("");
-        b4.setText("");
-        b5.setText("");
-        b6.setText("");
-        b7.setText("");
-        b8.setText("");
-        b9.setText("");
+        for(Button b : buttons){
+            b.setText("");
+        }
     }
 
     @Override
@@ -50,168 +39,39 @@ public class tttActivity extends AppCompatActivity {
     }
     public void start(View view){
         setContentView(R.layout.activity_ttt);
-        b1=(Button) findViewById(R.id.b1);
-        b2=(Button) findViewById(R.id.b2);
-        b3=(Button) findViewById(R.id.b3);
-        b4=(Button) findViewById(R.id.b4);
-        b5=(Button) findViewById(R.id.b5);
-        b6=(Button) findViewById(R.id.b6);
-        b7=(Button) findViewById(R.id.b7);
-        b8=(Button) findViewById(R.id.b8);
-        b9=(Button) findViewById(R.id.b9);
+        buttons = new Button[]{
+            (Button) findViewById(R.id.b1),
+            (Button) findViewById(R.id.b2),
+            (Button) findViewById(R.id.b3),
+            (Button) findViewById(R.id.b4),
+            (Button) findViewById(R.id.b5),
+            (Button) findViewById(R.id.b6),
+            (Button) findViewById(R.id.b7),
+            (Button) findViewById(R.id.b8),
+            (Button) findViewById(R.id.b9)
+        };
         turn=1;
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b1.getText().toString().equals("")){
-                    if (turn==1) {
-                        b1.setText("X");
-                        turn = 2;
-                    }
 
-                    else if (turn==2) {
-                        b1.setText("O");
-                        turn = 1;
-                    }
+        for(final Button b : buttons){
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setButtonText(b);
                 }
-                checkGame();
+            });
+        }
+    }
+    public void setButtonText(Button b){
+        if (b.getText().toString().equals("")){
+            if (turn==1) {
+                b.setText("X");
+                turn = 2;
             }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b2.getText().toString().equals("")){
-                    if (turn==1) {
-                        b2.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b2.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-
+            else if (turn==2) {
+                b.setText("O");
+                turn = 1;
             }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b3.getText().toString().equals("")){
-                    if (turn==1) {
-                        b3.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b3.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b4.getText().toString().equals("")){
-                    if (turn==1) {
-                        b4.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b4.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b5.getText().toString().equals("")){
-                    if (turn==1) {
-                        b5.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b5.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b6.getText().toString().equals("")) {
-                    if (turn == 1) {
-                        b6.setText("X");
-                        turn = 2;
-                    } else if (turn == 2) {
-                        b6.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b7.getText().toString().equals("")){
-                    if (turn==1) {
-                        b7.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b7.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b8.getText().toString().equals("")){
-                    if (turn==1) {
-                        b8.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b8.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (b9.getText().toString().equals("")){
-                    if (turn==1) {
-                        b9.setText("X");
-                        turn = 2;
-                    }
-
-                    else if (turn==2) {
-                        b9.setText("O");
-                        turn = 1;
-                    }
-                }
-                checkGame();
-            }
-        });
+        }
+        checkGame();
     }
 }
